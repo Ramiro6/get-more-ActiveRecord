03d245618ebca5e8135c8fbef44dae8f7cce6917
Ramiro6 <ramiroruiz578@gmail.com>
2018-03-12 17:02:28 +0100

    scope method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+
+    # default_scope { where.not(price: nil) }
+
+    default_scope { order(id: :desc) }
+
+    scope :recent, lambda { where(["created_at >= ?", 1.year.ago]) }
 end
